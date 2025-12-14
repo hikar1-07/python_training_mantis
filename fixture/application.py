eea2cf2a88cc92206ec194b2272bd4e787f7fed3
hikar1-07 <81418203+hikar1-07@users.noreply.github.com>
2025-12-14 22:45:37 +0300

    Задание №26: Реализовать проверки списков проектов через SOAP - Simple Object Access Protocol (SOAP)

--- fixture/application.py
+++ fixture/application.py
@@ -6,6 +6,7 @@ from fixture.project import ProjectHelper
 from fixture.james import JamesHelper
 from fixture.signup import SignupHelper
 from fixture.mail import MailHelper
+from fixture.soap import SoapHelper
 
 
 class Application:
@@ -29,6 +30,7 @@ class Application:
         self.config = config
         self.signup = SignupHelper(self)
         self.mail = MailHelper(self)
+        self.soap = SoapHelper(self)
 
     def is_valid(self):
         try:
